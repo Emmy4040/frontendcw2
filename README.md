2ab1db7e2c33bb8c577c89178678cc893361fe98
Emmy4040 <93773448+Emmy4040@users.noreply.github.com>
2022-02-19 00:15:04 +0400

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# CourseW1 github pages link: https://emmy4040.github.io/bata/coursework1/index.html
---
+++ products.js
@@ -0,0 +1,82 @@
+let lessons = [
+    {
+        "id": 001,
+        "subject": "Baking",
+        "location": "Dubai",
+        "price": 1000,
+        "availableInventory": 5,
+        "image": "images/baking.jfif"
+    },
+    {
+        "id": 002,
+        "subject": "Computer",
+        "location": "New York",
+        "price": 1100,
+        "availableInventory": 5,
+        "image": "images/computer.jfif"
+    },
+    {
+        "id": 003,
+        "subject": "Cooking",
+        "location": "Athens",
+        "price": 1200,
+        "availableInventory": 5,
+        "image": "images/cooking.png"
+    },
+    {
+        "id": 004,
+        "subject": "Dance",
+        "location": "Brussels",
+        "price": 1300,
+        "availableInventory": 5,
+        "image": "images/dance.png"
+    },
+    {
+        "id": 005,
+        "subject": "Gymnastics",
+        "location": "London",
+        "price": 1400,
+        "availableInventory": 5,
+        "image": "images/gymnastics.jfif"
+    },
+    {
+        "id": 006,
+        "subject": "Martial Arts",
+        "location": "Paris",
+        "price": 1500,
+        "availableInventory": 5,
+        "image": "images/martial arts.jfif"
+    },
+    {
+        "id": 007,
+        "subject": "Music",
+        "location": "Tokyo",
+        "price": 1600,
+        "availableInventory": 5,
+        "image": "images/music.jfif"
+    },
+    {
+        "id": 071,
+        "subject": "New Language",
+        "location": "Delhi",
+        "price": 1700,
+        "availableInventory": 5,
+        "image": "images/new language.jfif"
+    },
+    {
+        "id": 081,
+        "subject": "Sports",
+        "location": "Shanghai",
+        "price": 1800,
+        "availableInventory": 5,
+        "image": "images/sports.jpg"
+    },
+    {
+        "id": 010,
+        "subject": "Swimming",
+        "location": "Cairo",
+        "price": 1900,
+        "availableInventory": 5,
+        "image": "images/swimming.jpg"
+    },
+]
